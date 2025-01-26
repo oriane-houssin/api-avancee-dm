b45229d255259ec59af1992ad0f64091270912d1
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2025-01-26 16:57:46 +0100

    debut fonction signin

--- src/controller/authcontroller.js
+++ src/controller/authcontroller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config/key");
 
 exports.signup = async (req, res) => {
     const user = new User({
@@ -10,9 +12,35 @@ exports.signup = async (req, res) => {
     });
     try {
         await user.save();
-        res.send({ message: "User was successfully registered!" });
+        res.send({message: "User was successfully registered!"});
     } catch (error) {
         console.log(error);
         res.status(500).send("Erreur lors de la création du compte");
     }
 };
+
+exports.signin = async (req, res) => {
+    const user = await User.findOne({email: req.body.email});
+    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+    if (!passwordIsValid) {
+        return res.status(401).send({
+            accessToken: null,
+            message: "Incorrect password",
+        });
+    }
+    const token = jwt.sign({id: user.id, firstname: user.firstname, lastname: user.lastname},
+        config.secret,
+        {
+            algorithm: "HS256",
+            allowInsecureKeySizes: true,
+            expiresIn: 86400, // la session expire dans 24h
+        });
+    res.status(200).send({
+        id: user._id,
+        email: user.email,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        accessToken: token,
+    });
+}
