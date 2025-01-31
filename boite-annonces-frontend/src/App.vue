93311e6a4a363b5932cabb7d0a4e80f283b01be1
MateoMBR <m.burnichon.pro@gmail.com>
2025-01-31 15:08:39 +0100

    menu dépliant

--- boite-annonces-frontend/src/App.vue
+++ boite-annonces-frontend/src/App.vue
@@ -5,19 +5,22 @@
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <router-link class="nav-link" to="/signup">Signup</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/signin">Signin</router-link>
-          </li>
-          <li class="nav-item">
             <router-link class="nav-link" to="/announcements">Announcements</router-link>
           </li>
           <li class="nav-item">
             <router-link class="nav-link" to="/create-announcement">Create Announcement</router-link>
           </li>
         </ul>
-        <button class="btn btn-outline-danger" @click="logout">Logout</button>
+        <div class="dropdown ml-auto">
+          <button class="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Account
+          </button>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+            <router-link v-if="!isLoggedIn" class="dropdown-item" to="/signup">Signup</router-link>
+            <router-link v-if="!isLoggedIn" class="dropdown-item" to="/signin">Signin</router-link>
+            <button v-if="isLoggedIn" class="dropdown-item" @click="logout">Logout</button>
+          </div>
+        </div>
       </div>
     </nav>
     <div class="container mt-4">
@@ -29,18 +32,26 @@
 <script>
 import axios from 'axios';
 import Cookies from 'js-cookie';
+import { ref, onMounted, watch } from 'vue';
 import { useRouter } from 'vue-router';
 
 export default {
   name: 'App',
   setup() {
     const router = useRouter();
+    const isLoggedIn = ref(false);
+
+    const checkAuth = () => {
+      const token = Cookies.get('accessToken');
+      isLoggedIn.value = !!token;
+    };
 
     const logout = async () => {
       try {
         await axios.post('http://localhost:3000/api/auth/logout', {}, { withCredentials: true });
         Cookies.remove('accessToken');
         alert('Logout successful');
+        isLoggedIn.value = false;
         router.push('/signin');
       } catch (error) {
         console.error('Error logging out:', error);
@@ -48,7 +59,18 @@ export default {
       }
     };
 
+    onMounted(() => {
+      checkAuth();
+    });
+
+    watch(isLoggedIn, (newVal) => {
+      if (!newVal) {
+        router.push('/signin');
+      }
+    });
+
     return {
+      isLoggedIn,
       logout,
     };
   },
--- boite-annonces-frontend/src/main.js
+++ boite-annonces-frontend/src/main.js
@@ -1,9 +1,5 @@
-// filepath: /Users/mateoburnichon/api-avancee-dm/boite-annonces-frontend/src/main.js
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+createApp(App).use(router).mount('#app');
